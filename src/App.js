76ad1cf7300768b68c0eb886c9c1d9fc4bc5b14e
mohsen.zergani <mohsen.zergani@gmail.com>
2021-09-04 15:08:30 +0430

    Page Userlist

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import Home from "./Pages/Home/Home";
 import "./App.css";
+import UserList from "./Pages/UserList/UserList";
+import User from "./Pages/User/User";
 const App = () => {
   return (
     <>
@@ -14,6 +16,12 @@ const App = () => {
           <Switch>
             <Route path="/" exact>
               <Home />
+            </Route>
+            <Route path="/users">
+              <UserList />
+            </Route>
+            <Route path="/user/:userId">
+              <User />
             </Route>
           </Switch>
         </div>
